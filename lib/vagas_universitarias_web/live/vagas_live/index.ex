6c2314cd3f5a591d611b0723e6a7398158a83e9f
otaviouu <otavio.venancio.oliveira@usp.br>
2025-10-29 18:17:12 -0300

    Refactor VagasLive.Index to improve structure and enhance readability by extracting actions and job list into separate functions

--- lib/vagas_universitarias_web/live/vagas_live/index.ex
+++ lib/vagas_universitarias_web/live/vagas_live/index.ex
@@ -6,12 +6,10 @@ defmodule VagasUniversitariasWeb.VagasLive.Index do
   on_mount {VagasUniversitariasWeb.LiveUserAuth, :live_user_optional}
 
   def mount(_params, _session, socket) do
-    vagas = Vagas.list_vagas!(load: [:empresa, :salario_reais])
-
     socket =
       socket
-      |> assign(page_title: "Vagas Universitárias")
-      |> assign(vagas: vagas)
+      |> set_title("Vagas Universitárias")
+      |> load_vagas
       |> assign(:current_user, Map.get(socket.assigns, :current_user))
 
     {:ok, socket}
@@ -20,51 +18,85 @@ defmodule VagasUniversitariasWeb.VagasLive.Index do
   def render(assigns) do
     ~H"""
     <Layouts.app {assigns}>
-      <div class="flex items-center space-x-4 mb-4">
-        <.button
-          :if={Vagas.can_create_vaga?(@current_user)}
-          phx-click={JS.navigate(~p"/vagas/new")}
-          class="btn btn-primary"
-        >
-          <.icon name="hero-plus" class="w-4 h-4 mr-2" /> Nova Vaga
-        </.button>
-        <.button
-          :if={Vagas.can_create_vaga?(@current_user)}
-          phx-click={JS.navigate(~p"/empresas/new")}
-          class="btn btn-primary"
-        >
-          <.icon name="hero-plus" class="w-4 h-4 mr-2" /> Nova Empresa
-        </.button>
-      </div>
-      <content>
-        <ul class="list bg-base-100 rounded-box shadow-md">
-          <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Vagas mais recentes</li>
-
-          <li
-            :for={vaga <- @vagas}
-            class="list-row hover:bg-base-200"
-            phx-click={JS.navigate(~p"/vagas/#{vaga.id}")}
-          >
-            <div>
-              <img
-                class="size-10 rounded-box"
-                src={vaga.empresa.logo_url}
-              />
-            </div>
-            <div class="flex flex-col flex-1 gap-1">
-              <div>
-                {vaga.empresa.nome}: {vaga.titulo} - <span class="text-secondary">{vaga.tipo}</span>
-              </div>
-              <div class="flex gap-2">
-                <div :for={_tags <- 1..3} class="badge badge-soft badge-secundary">tag</div>
-              </div>
-            </div>
-            <div class="badge badge-soft badge-primary">R$ {vaga.salario_reais}</div>
-            <div class="badge badge-outline">São Paulo - SP</div>
-          </li>
-        </ul>
-      </content>
+      <.actions current_user={@current_user} />
+      <.async_result :let={vagas} assign={@vagas}>
+        <:loading>
+          <.loading_spinner />
+        </:loading>
+        <:failed :let={reason}>{reason}</:failed>
+        <.job_list vagas={vagas} />
+      </.async_result>
     </Layouts.app>
     """
+  end
+
+  def actions(assigns) do
+    ~H"""
+    <div class="flex items-center space-x-4 mb-4">
+      <.button
+        :if={Vagas.can_create_vaga?(@current_user)}
+        phx-click={JS.navigate(~p"/vagas/new")}
+        class="btn btn-primary"
+      >
+        <.icon name="hero-plus" class="w-4 h-4 mr-2" /> Nova Vaga
+      </.button>
+      <.button
+        :if={Vagas.can_create_vaga?(@current_user)}
+        phx-click={JS.navigate(~p"/empresas/new")}
+        class="btn btn-primary"
+      >
+        <.icon name="hero-plus" class="w-4 h-4 mr-2" /> Nova Empresa
+      </.button>
+    </div>
+    """
+  end
+
+  def job_list(assigns) do
+    ~H"""
+    <ul class="list bg-base-100 rounded-box shadow-md">
+      <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Vagas mais recentes</li>
+
+      <li
+        :for={vaga <- @vagas}
+        class="list-row hover:bg-base-200"
+        phx-click={JS.navigate(~p"/vagas/#{vaga.id}")}
+      >
+        <div>
+          <img
+            class="size-10 rounded-box"
+            src={vaga.empresa.logo_url}
+          />
+        </div>
+        <div class="flex flex-col flex-1 gap-1">
+          <div>
+            {vaga.empresa.nome}: {vaga.titulo} - <span class="text-secondary">{vaga.tipo}</span>
+          </div>
+          <div class="flex gap-2">
+            <div :for={_tags <- 1..3} class="badge badge-soft badge-secundary">tag</div>
+          </div>
+        </div>
+        <div class="badge badge-soft badge-primary">R$ {vaga.salario_reais}</div>
+        <div class="badge badge-outline">São Paulo - SP</div>
+      </li>
+    </ul>
+    """
+  end
+
+  def loading_spinner(assigns) do
+    ~H"""
+    <div class="flex justify-center py-8">
+      <span class="loading loading-spinner text-primary"></span>
+    </div>
+    """
+  end
+
+  defp set_title(socket, title) do
+    assign(socket, :page_title, title)
+  end
+
+  defp load_vagas(socket) do
+    assign_async(socket, :vagas, fn ->
+      {:ok, %{vagas: Vagas.list_vagas!(load: [:empresa, :salario_reais])}}
+    end)
   end
 end
